242d21b942bd3d821c696f48daa17823c24ee9a8
Carlos Estrada <carlestrada@deloitte.ca>
2019-10-24 16:56:54 -0400

    Grid and Flex Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './GridFlexLayout';
+import GridFlexLayout from './GridFlexLayout';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <GridFlexLayout/>
     </div>
   );
 }
